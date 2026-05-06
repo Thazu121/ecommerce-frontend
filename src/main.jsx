885279fd57c231ec8f15c9c810c564970b6492ab
Thasleema <thasleemapk133@gmail.com>
2026-05-06 11:56:16 +0530

    add setup for frontend

---
+++ src/main.jsx
@@ -0,0 +1,42 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {Provider} from "react-redux"
+import { store } from './redux/store.js'
+
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<RootLayout />,
+    // errorElement
+    children:[
+      {index:true,element:<Home />},
+      {path:"products",element:<ProductSection />,loader:ProductLoader,hydrateFallbackElement:<p>Loading...</p>},
+      {path:"contact", element:<Contact />},
+      {path:"product/:productId",
+      element:(<ProtectRoute>
+    <SingleViewProduct />
+    </ProtectRoute>),
+        loader:SingleProductLoader},
+      { path: "cart", element: 
+      <ProtectRoute>
+        <Cart /> 
+      </ProtectRoute>},
+      {path:"Login",element:<AdminLogin/>}
+
+
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <Provider store={store}>
+  <RouterProvider router={router} />
+  </Provider>
+  </StrictMode>
+)
+
+
